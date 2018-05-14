ec233908d3c9f2cc8ba84d3cd2482f5a90a53a23
Kandelion <rlagusdn6575@gmail.com>
2018-05-14 15:21:46 +0000

    Added JSON parser lib

--- prj_translation/main.cpp
+++ prj_translation/main.cpp
@@ -1,4 +1,6 @@
 #include "curl/curl.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writter.h"
 #include <fstream>
 #include <sstream>
 #include <iostream>
@@ -6,6 +8,8 @@
 #include <malloc.h>
 
 #define MAX_URL 1024
+
+using namespace rapidjson;
 
 // callback function writes data to a std::ostream
 static size_t data_write(void* buf, size_t size, size_t nmemb, void* userp)
@@ -105,14 +109,15 @@ int main()
 	if(CURLE_OK == curl_read(URL, ofs))
 	{
 		// Web page successfully written to file
-	}
+	}*/
 
 	std::ostringstream oss;
+	std::string result;
 	if(CURLE_OK == curl_read(URL, oss))
 	{
 		// Web page successfully written to string
-		std::string html = oss.str();
-	}*/
+		result = oss.str();
+	}
 
 	std::cout<<"==Standard Output=="<<std::endl;
 	std::cout<<"URL : "<<URL<<std::endl;
